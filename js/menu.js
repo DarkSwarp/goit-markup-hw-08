f517c13ff58fa81b85c07e84945467139d39e29c
DarkSwarp <mihkleban@gmail.com>
2022-11-21 00:04:09 +0200

    майже зроблене дз, залишилось розібратися зі зняттям блокування скролу

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,20 @@
 (() => {
     const refs = {
-        openMenuBtn: document.querySelector("[data-menu-open]"),
-        closeMenuBtn: document.querySelector("[data-menu-close]"),
+        moveMenuBtn: document.querySelector("[data-menu-move]"),
         menu: document.querySelector("[data-menu]"),
         body: document.querySelector("body"),
+        navList: document.querySelector(".nav__list"),
     };
 
-    refs.openMenuBtn.addEventListener("click", toggleMenu);
-    refs.closeMenuBtn.addEventListener("click", toggleMenu);
+    refs.moveMenuBtn.addEventListener("click", toggleMenu);
+    refs.navList.addEventListener("click", removeMenu);
 
     function toggleMenu() {
         refs.menu.classList.toggle("is-hidden");
+        refs.body.classList.toggle("no-scroll");
+    }
+    function removeMenu() {
+        refs.menu.classList.add("is-hidden");
         refs.body.classList.toggle("no-scroll");
     }
 })();
